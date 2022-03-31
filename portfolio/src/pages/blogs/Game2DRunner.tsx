6bb1197a32b508f8b753e4185b3aa048972efb38
Camrynlangdon <44718126+Camrynlangdon@users.noreply.github.com>
2022-03-30 19:00:51 -0700

    wip

--- portfolio/src/pages/blogs/Game2DRunner.tsx
+++ portfolio/src/pages/blogs/Game2DRunner.tsx
@@ -4,7 +4,17 @@ import Styled from 'styled-components';
 import Blog from '../../views/Blog'
 
 
+const GameDiv = Styled.div`
+    position: relative;
+    
 
+    iframe {
+        
+        width: 100%;
+        max-height: 90%
+    }
+
+`
 
 
 
@@ -16,11 +26,18 @@ function Game2DRunner () {
     >
         <h2>Used WASD to move {'&'} SPACE BAR to double jump</h2>
         
-        {/* <iframe  src="https://itch.io/embed-upload/5507806?color=333333"  width="980" height="580"><a href="https://camrynlangdon.itch.io/2d-runner">Play 2D Runner on itch.io</a></iframe>
-         */}
         
-        <iframe frameBorder="0" src="https://itch.io/embed-upload/5508117?color=333333" allowFullScreen width="1000" height="950"><a href="https://camrynlangdon.itch.io/2d-runner">Play 2D Runner on itch.io</a></iframe>
+        <GameDiv>
+
+        <iframe frameBorder="0" 
+        src="https://itch.io/embed-upload/5508117?color=333333" 
+        allowFullScreen width="1000" height="950">
+        <a href="https://camrynlangdon.itch.io/2d-runner">
+        Play 2D Runner on itch.io</a>
+        </iframe>
          
+        </GameDiv>
+
     </Blog>
 }
 
